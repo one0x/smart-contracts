580cb05952c3e669b6c8347345eaec8525ef1e71
donatedTunic1290 <connect@aakashbansal.com>
2018-05-20 15:40:43 +0530

    - Updated config

--- truffle.js
+++ truffle.js
@@ -10,7 +10,9 @@ module.exports = {
 		testnet: {
 			host: "127.0.0.1",
 			port: 8545,
-			network_id: "2"
+			network_id: "2",
+			from: '0x742ba4f0cd93b3b7876a54ef10838973ebadcf49',
+			gas: 9000000
 		}
 	}
 };
